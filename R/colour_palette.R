0feef0138ad9b2fa507ecbea7ddda7b43cb8086d
rprendergast405 <rprendergast405@gmail.com>
2016-07-06 11:53:38 +1200

    Marketview Colour Palette
     * Created a stand-alone script for these objects

---
+++ R/colour_palette.R
@@ -0,0 +1,29 @@
+# colour_palette.R
+# -------------------------------------------------------------------------
+# A script containing some colours in the Marketview palette
+# -------------------------------------------------------------------------
+# Created by Bert on 06-07-2016
+# -------------------------------------------------------------------------
+# Last edited by Bert on 06-07-2016
+# -------------------------------------------------------------------------
+
+# Marketview Colour Palettes ----
+# primary
+mvl_blue <- grDevices::rgb(65, 90, 104, maxColorValue = 255)
+mvl_pewter <- grDevices::rgb(152, 148, 130, maxColorValue = 255)
+# background
+mvl_grey <- grDevices::rgb(128, 130, 133, maxColorValue = 255)
+# trend indicators
+mvl_red <- grDevices::rgb(195, 39, 47, maxColorValue = 255)
+mvl_orange <- grDevices::rgb(248, 152, 29, maxColorValue = 255)
+mvl_green <- grDevices::rgb(120, 162, 47, maxColorValue = 255)
+# qualitative
+mvl_teal <- grDevices::rgb(0, 160, 175, maxColorValue = 255)
+mvl_half_teal <- grDevices::rgb(185, 227, 230, maxColorValue = 255)
+mvl_navy <- grDevices::rgb(9, 52, 122, maxColorValue = 255)
+mvl_royal <- grDevices::rgb(0, 119, 192, maxColorValue = 255)
+mvl_purple <- grDevices::rgb(104, 89, 145, maxColorValue = 255)
+mvl_half_purple <- grDevices::rgb(204, 203, 226, maxColorValue = 255)
+mvl_half_grey <- grDevices::rgb(190, 192, 194, maxColorValue = 255)
+mvl_hay <- grDevices::rgb(233, 214, 102, maxColorValue = 255)
+mvl_half_hay <- grDevices::rgb(242, 234, 176, maxColorValue = 255)
