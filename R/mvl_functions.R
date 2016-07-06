9b9c79c7112911fa11dde2faf9d769267a202743
rprendergast405 <rprendergast405@gmail.com>
2016-07-07 09:48:05 +1200

    map_get header edited

--- R/mvl_functions.R
+++ R/mvl_functions.R
@@ -98,13 +98,16 @@ LaTeX: pdfLaTeX"
 #' @param map_dir Where are the RData files located?
 #'
 #' @return \code{map.df} A data frame describing the polygons with the following atributes:
-#'      \itemise{
-#'      \item long
-#'      \item lat
-#'      \item group
-#'      \item CAU
-#'      \item CAU_NAME
+#'      \itemize{
+#'      \item long (longitude of the boundary point)
+#'      \item lat (latitude of the boundary point)
+#'      \item group (polygon grouping)
+#'      \item CAU (CAU number)
+#'      \item CAU_NAME (CAU name)
 #'      }
+#' @examples map_get("Auckland")
+#' ggplot(map.df) +
+#'  geom_polygon(aes(x = long, y = lat, group = group))
 #' @export map_get
 map_get <- function(city = "Auckland", census_version = 2013, map_dir = "M:/R/map_data") {
 
