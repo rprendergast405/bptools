bacdc08c0648ee0ab6f94ed024a530f0bfa62bc0
rprendergast405 <rprendergast405@gmail.com>
2017-02-24 10:51:50 +1300

    Added flextable_negatives() and flextable_leaf_joined()

--- R/ReporteRs functions.R
+++ R/ReporteRs functions.R
@@ -38,6 +38,9 @@ flextable_teal <- function(df, size = 12, padding = size / 2) {
                                           outer.vertical = ReporteRs::borderProperties( color = "white", style = "solid" ),
                                           outer.horizontal = ReporteRs::borderProperties( color = "white", style = "solid"))
 
+  # Add the original values to the flextable for reference
+  df_ft$vals <- df
+
   return(df_ft)
 }
 
@@ -72,6 +75,9 @@ flextable_dark <- function(df, size = 12, padding = size / 2) {
                                           inner.horizontal = ReporteRs::borderProperties( color = "white", style = "solid" ),
                                           outer.vertical = ReporteRs::borderProperties( color = "white", style = "solid" ),
                                           outer.horizontal = ReporteRs::borderProperties( color = "white", style = "solid"))
+
+  # Add the original values to the flextable for reference
+  df_ft$vals <- df
 
   return(df_ft)
 }
@@ -163,6 +169,9 @@ flextable_black <- function(df, size = 12, padding = size / 2) {
                                           outer.vertical = ReporteRs::borderProperties( color = "white", style = "solid" ),
                                           outer.horizontal = ReporteRs::borderProperties( color = "white", style = "solid"))
 
+  # Add the original values to the flextable for reference
+  df_ft$vals <- df
+
   return(df_ft)
 }
 
@@ -198,5 +207,95 @@ flextable_leaf <- function(df, size = 12, padding = size / 2) {
                                           outer.vertical = ReporteRs::borderProperties( color = "white", style = "solid" ),
                                           outer.horizontal = ReporteRs::borderProperties( color = "white", style = "solid"))
 
+  # Add the original values to the flextable for reference
+  df_ft$vals <- df
+
   return(df_ft)
+}
+
+
+#' Create a FlexTable with Multiple Header Rows
+#'
+#' Format and print a joined FlexTable
+#'
+#' @param df A \code{data.frame} of values
+#' @param pr Pseudo-rows: Indices of the header cells in df to be merged
+#' @param hr Header Rows: Indices of the rows to apply header formatting
+#' @param size Font size in pt
+#' @param padding Cell padding
+#'
+#' @return df_ft A FlexTable of the df with formatting
+#' @export flextable_leaf_joined
+flextable_leaf_joined <- function(df, pr, hr,
+                                  size = 12, padding = size/2){
+
+  #Create FT
+  df_ft <- ReporteRs::FlexTable(df,
+                                header.columns = F,
+                                body.cell.props = ReporteRs::cellProperties(padding.top = padding,
+                                                                            padding.bottom = padding), body.par.props = ReporteRs::parProperties(text.align = "center"),
+                                body.text.props = ReporteRs::textProperties(color = "black",
+                                                                            font.size = size, font.family = "Helvetica Neue"))
+
+  df_ft <- ReporteRs::setZebraStyle(df_ft, odd = grDevices::rgb(236,
+                                                                241, 233, maxColorValue = 255), even = grDevices::rgb(214,
+                                                                                                                      226, 207, maxColorValue = 255))
+
+  #If necessary join heading rows
+  if(!is.null(pr)){
+    df_ft %>%
+      ReporteRs::spanFlexTableColumns(i = pr, runs = as.character(df[pr,]))
+  }
+
+  #Format the heading rows
+  df_ft[i = hr] <- ReporteRs::cellProperties(background.color = marketview::mvl_leaf,
+                                             padding.top = padding, padding.bottom = padding)
+
+  df_ft[i = hr] <- ReporteRs::textProperties(color = "white",
+                                             font.size = size, font.family = "Helvetica Neue", font.weight = "bold")
+
+  #Format borders
+  df_ft <- ReporteRs::setFlexTableBorders(df_ft, inner.vertical = ReporteRs::borderProperties(color = "white",
+                                                                                              style = "solid"), inner.horizontal = ReporteRs::borderProperties(color = "white",
+                                                                                                                                                               style = "solid"), outer.vertical = ReporteRs::borderProperties(color = "white",
+                                                                                                                                                                                                                              style = "solid"), outer.horizontal = ReporteRs::borderProperties(color = "white",
+                                                                                                                                                                                                                                                                                               style = "solid"))
+
+  return(df_ft)
+}
+
+
+
+#' Highlight Negative Cell Values in Red
+#'
+#' A function to highlight any negative values in a FlexTable for given column names.
+#' This should work regardless of the formatting - character values such as those returned by \code{marketview::dollar()} or
+#' \code{marketview::percent()} will be dealt with if I've done this right.
+#'
+#' @param ftbl A FlexTable object to format
+#' @param col_names The names of the columns in which to highlight the negatives
+#'
+#' @return ftbl The original FlexTable with the formatting applied
+#' @export flextable_negatives
+flextable_negatives <- function(ftbl, col_names) {
+
+  # Get the original data.frame from the flextable object
+  df <- ftbl$vals
+
+  for(column in col_names) {
+    # Get the specified columns
+    col_ind <- which(ftbl$col_id == column)
+    col_vals <- df[, col_ind]
+
+    # Get the values as numerics
+    col_vals <-  gsub("[^-\\.0-9]", "", col_vals)
+    col_vals <- as.numeric(col_vals)
+
+    # Find any values that are less than zero
+    neg_ind <- which(col_vals < 0)
+
+    # Highlight the negative values in the flextable
+    ftbl <- marketview::flextable_cell_bold(ftbl, neg_ind, col_ind, marketview::mvl_red)
+  }
+  return(ftbl)
 }
