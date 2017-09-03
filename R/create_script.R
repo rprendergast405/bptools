3aab4132428406edbde19fd28548b5c0a5621c9c
rprendergast405 <rprendergast405@gmail.com>
2017-09-04 09:54:18 +1200

    create_script() bug fixes

--- R/create_script.R
+++ R/create_script.R
@@ -8,8 +8,11 @@
 #' @param location Directory where the script should be created - default is the /R directory
 #'
 #' @export create_script
-create_script <- function(name, project_name = getwd(), location = "R") {
-  script_text <- c("# ", name, ".R
+create_script <- function(name, project_dir = getwd(), location = "R") {
+
+  project_name <- gsub("^.*/", "", project_dir)
+
+script_text <- c("# ", name, ".R
 # Created by ", Sys.info()["user"], ", on ", format(Sys.Date(), "%d %m %Y"), "
 # -------------------------------------------------------------------------
 # A script designed to ", name, " for the ", project_name, " project
@@ -25,12 +28,12 @@ create_script <- function(name, project_name = getwd(), location = "R") {
 
 # 0. INITIALISE -----------------------------------------------------------
 
-source(file.path(\"R\", \"0 ", project_name, " initialise.R\"))
+source(file.path(\"", project_dir, "\", \"R\", \"0 ", project_name, " initialise.R\"))
 
 # 1. IMPORT DATA ----------------------------------------------------------
 
 data_import()
 ")
 
-  cat(paste(script_text, collapse = ""), file = file.path(project_name, location, paste0(name, ".R")))
+  cat(paste(script_text, collapse = ""), file = file.path(project_dir, location, paste0(name, ".R")))
 }
