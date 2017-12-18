9e6163856a4a9f074a1bdf9f04298a031a4c6d61
rprendergast405 <rprendergast405@gmail.com>
2017-12-19 12:53:16 +1300

    Added more defaults to the initialise script which should help with consistency

--- R/create_project.R
+++ R/create_project.R
@@ -146,10 +146,6 @@ create_init_script <- function(project_name, root_dir) {
 
 # 0. INITIALISE -----------------------------------------------------------
 
-# clear the workspace ----
-rm(list = setdiff(ls(), c(\"base_dir\", \"archive\")))
-gc()
-
 # import packages -----
 library(RODBC)
 library(tools)
@@ -172,10 +168,10 @@ fig_dir <- file.path(output_dir, \"figures\")
 tab_dir <- file.path(output_dir, \"tables\")
 
 # Source any function scripts -----
-source_dir(file.path(\"R/functions\"))
+source_dir(file.path(\"R\", \"functions\"))
 
 # Database connections -----
-DB <- odbcConnect(dsn = \"MVIEW\", uid = \"bespoke\", pwd = \"bespoke\")
+DB <- odbcConnect(dsn = \"MVIEW\", uid = \"bespoke\", pwd = \"bespoke\", believeNRows = FALSE)
 
 # Set the plot theme -----
 theme_set(theme_mvl())
@@ -185,6 +181,12 @@ scale_colour_discrete <- partial(scale_colour_mvl, palette = \"mcd\")
 scale_colour_continuous <- scale_colour_mvlc
 scale_fill_discrete <- partial(scale_fill_mvl, palette = \"mcd\")
 scale_fill_continuous <- scale_fill_mvlc
+
+grDevices::windowsFonts(calb = \"Calibri Bold\",
+                        cal = \"Calibri\",
+                        hn = \"Helvetica Neue\",
+                        hnb = \"Helvetica Neue Bold\",
+                        cen = \"Century Gothic\")
 
 # Set ReporteRs defaults
 options(\"ReporteRs-default-font\" = \"Helvetica Neue\",
@@ -196,6 +198,15 @@ print.data.frame <- function(x, ..., n = NULL, width = NULL) {
 x <- dplyr::as.tbl(x)
 print(x, ..., n = NULL, width = NULL)
 }
+# Set stringsAsFactors to FALSE
+options(\"stringsAsFactors\" = FALSE,
+        max.print\" = 200)
+
+library(httr)
+options(RCurlOptions = list(proxy = \"proxy.private.marketview.co.nz:3128\"))
+options(rsconnect.http = \"rcurl\")
+set_config(use_proxy(url = \"proxy.private.marketview.co.nz\", port = 3128))
+
 ")
 
   cat(paste(init_text, collapse = ""), file = file.path(root_dir, "R", paste0("0 ", project_name, " initialise.R")))
@@ -456,9 +467,6 @@ if(data_refresh) {
 
 # 2. PRODUCE THE RESULTS --------------------------------------------------
 
-# Archive the old results ----
-output_archive()
-
 # Re-run the results script ----
 source(file.path(\"R\", \"2 ", project_name, " results.R\"))
 
@@ -474,102 +482,3 @@ source(file.path(\"R\", \"3 ", project_name, " report.R\"))
 }
 
 
-create_sp_fn <- function(root_dir) {
-
-sp_text <- "# file_spellchecker.R
-#A function that will check for spelling errors in strings manually entered in a script
-#This returns two data.frames, one with a row for each string and a column for each mistake,
-#The second is a summary of each unique mistake and the number of times it appears in the first df
-#(Note the second df only counts one occurrences of each error in each string, hence it gives the number
-#of strings a particular error occurs in, but not necessarily the total number of errors to be fixed)
-
-
-file_spellchecker <- function(file_connection, quote_type = c(\'double\', \'both\', \'single\'), ...){
-
-match.arg(quote_type)
-
-#Read the file
-flines <- readLines(file_connection)
-
-#Match all strings encased in single or double quotations
-#i.e. (those that were manually entered by the user)
-dq_strings <- str_match(flines, \"\\\"(.*?)\\\"\")[,1]
-sq_strings <- str_match(flines, \"\\\'(.*?)\\\'\")[,1]
-
-#Create strings to check
-if(quote_type == \'both\'){
-  strings <- c(sq_errors[!is.na(sq_errors)], dq_errors[!is.na(dq_errors)])
-}
-if(quote_type == \'double\'){
-  strings <- dq_errors[!is.na(dq_errors)]
-}
-if(quote_type == \'single\'){
-  strings <- sq_errors[!is.na(sq_errors)]
-}
-
-#Match all acronyms, create a filter to remove these from spell check
-acronyms <- str_match(strings, \"[A-Z]{2,}\")
-acronyms <- unique(acronyms[!is.na(acronyms)])
-acronym_filt <- paste(acronyms, collapse = \"|\")
-
-#Find all spelling errors, use supply dots to change dictionaries
-errors <- strings %>%
-  gsub(acronym_filt, \"\", .) %>%
-  sapply(., qdap::which_misspelled, ...)
-
-errors <- errors[(1:length(errors))[as.logical(sapply(errors, function(x){!is.null(x)}))]]
-
-#Find the maximum number of mistakes in a single string
-max_mistakes <- errors %>%
-  sapply(., length) %>%
-  max
-
-#Create data.frame listing errors
-error_df <- data.frame(names(errors),  t(data.table::as.data.table(errors))[,1:max_mistakes]) %>%
-  setNames(c(\'strings\', paste0(\'error_\', 1:max_mistakes)))
-
-#If there is more than one error column, remove duplicate errors
-if(max_mistakes > 1){
-  for(i in 2:max_mistakes){
-    error_df <- remove_duplicate_errors(dat = error_df,  e_col = paste0(\'error_\', i))
-  }
-}
-
-#Create a summarised data.frame of the count of unique string x error combinations
-summarised_errors <- lapply(paste0(\'error_\', 1:max_mistakes), error_summarise, dat = error_df)
-summarised_errors <- lapply(summarised_errors, function(x){setNames(x, c(\'error\', \'count\'))})
-
-summarised_errors <- bind_rows(summarised_errors) %>%
-  group_by(error) %>%
-  summarise(count = sum(count)) %>%
-  filter(!is.na(error)) %>%
-  arrange(desc(count))
-
-output <- list(error_df, summarised_errors)
-
-return(output)
-
-}
-
-#Helper functions
-
-error_summarise <- function(dat, evar_str){
-
-  dat %>%
-    group_by_(evar_str) %>%
-    summarise(error_count = n()) %>%
-    arrange(desc(error_count))
-}
-
-remove_duplicate_errors <- function(dat, e_col){
-
-  dat[dat[,\'error_1\'] == dat[,e_col], e_col] <- NA
-
-  return(dat)
-
-}"
-
-cat(paste(sp_text, collapse = ""), file = file.path(root_dir, "R/functions/file_spellchecker.R"))
-
-
-}
