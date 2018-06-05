8b22d8485e9fbb572c486d8ef4e4e1568a5c8243
rprendergast405 <rprendergast405@gmail.com>
2018-06-06 10:57:20 +1200

    sum_spend()
    - convenience function for adding the spending

---
+++ R/sum_spend.R
@@ -0,0 +1,18 @@
+#' Add Spending across Given Attributes
+#'
+#' A convenience function for quickly adding the spending across attributes.
+#' \code{sum_spend(dat, att)} is shorthand for \code{dat %>% group_by(att) %>% summarise(SPEND = sum(SPEND, na.rm = TRUE)) %>% ungroup()}
+#'
+#' @param dat A data frame to add the spending
+#' @param ... Attributes to group by
+#'
+#' @export sum_spend
+sum_spend <- function(dat, ...) {
+  atts <- dplyr::quos(...)
+
+  dat <- dplyr::group_by(dat, !!!atts)
+  dat <- dplyr::summarise(dat, SPEND = sum(SPEND, na.rm = TRUE))
+  dat <- dplyr::ungroup(dat)
+
+  return(dat)
+}
